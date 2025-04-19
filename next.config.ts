e72696c3c0abe3e212461912124bf15f517f303b
Shashank <shashank78680@gmail.com>
2025-04-19 16:04:51 +0530

    next config image domain

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "github.com"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'private-user-images.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ]
   }
 };
